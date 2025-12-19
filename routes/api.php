6cf70e29210cc9834f1d23da4e6898f7569d57d3
ExA2040 <ehsan_alinezhad@ymail.com>
2025-12-19 13:40:38 +0330

    feat: add new routes project task comment

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('throttle:auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::apiResource('projects', ProjectController::class);
+    Route::apiResource('projects.tasks', TaskController::class)->except(['index'])->shallow();
+    Route::get('projects/{project}/tasks', [TaskController::class, 'index']);
+    Route::apiResource('tasks.comments', CommentController::class)->except(['index'])->shallow();
+    Route::get('tasks/{task}/comments', [CommentController::class, 'index']);
+
+});
